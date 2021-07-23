808afefaa658ff4ad281fc22aab35de174e38e48
xchen <chenxin199261@gmail.com>
2021-07-23 17:33:40 +0800

    creat main

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# LearnCPP_NN
+# Learn CPP by writing a neural network.
+I hope I can finish this learning process.
+
+*2021.7.23*
+1. Build main.cpp. Holding the main functions. 
+2. Learning the basic format.
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**  
+ * The main entrance of this program
+ * The common should be organized in CPP style. 
+ *
+ * @brief Entrance of the learning program.
+          This program is a neural-network program. 
+ * @author Xin Chen
+ *
+ * @date 2021/7/23 (create)  0 dev_edu
+ * @version 0 dev_edu
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello, world!" << endl;
+    return 0;
+}
